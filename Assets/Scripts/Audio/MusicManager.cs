f7b0d8a70b8478113b6bc8111a45d15436f56c46
Noah Girdler <noahgirdler@gmail.com>
2024-06-09 18:49:40 -0400

    Makes Changes to Scenes and Moves Wwise Project

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+
+    // MUSIC MANAGER VARIABLES
+    // private AK.Wwise.Event musicName = null;
+    // private AK.Wwise.RTPC RTPCName = null;
+
+
+    private void Awake()
+    {
+        // Initializes Wwise Soundbank
+        AkBankManager.LoadBank("Main", false, false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
